5fbdcf3bdb0b0e25d11d47bee1b53c752fd6549f
Prashant-Jain-Hiteshi <prashant.jain@hiteshi.com>
2025-07-18 10:14:50 +0530

    Allow origin all

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+   app.enableCors({
+     origin: '*',
+     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+     allowedHeaders: ['Content-Type', 'Authorization'],
+     credentials: true,
+   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
